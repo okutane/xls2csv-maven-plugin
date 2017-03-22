604e78bdb3fb635cc8ef1e38e9951abadfb98b8b
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-22 19:45:59 +0300

    try deploy from impl and tags

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'impl' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+  openssl aes-256-cbc -K $encrypted_eec546499c77_key -iv $encrypted_eec546499c77_iv -in codesigning.asc.enc -out codesigning.asc -d
+  gpg --fast-import cd/signingkey.asc
+fi
+
+if [ -n "$TRAVIS_TAG" ]; then
   openssl aes-256-cbc -K $encrypted_eec546499c77_key -iv $encrypted_eec546499c77_iv -in codesigning.asc.enc -out codesigning.asc -d
   gpg --fast-import cd/signingkey.asc
 fi
