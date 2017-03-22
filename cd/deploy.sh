ab274658c0855dff59d0b05906aabbdebabb9391
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-22 19:45:59 +0300

    try deploy from impl and tags

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'impl' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    mvn deploy -P sign --settings cd/mvnsettings.xml
+fi
+
+if [ -n "$TRAVIS_TAG" ]; then
     mvn deploy -P sign --settings cd/mvnsettings.xml
 fi
