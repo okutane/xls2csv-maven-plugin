4ab5d03aaf20ae4fc832917f82ea0f52d692930e
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-23 02:11:36 +0300

    cleanup

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'impl' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_eec546499c77_key -iv $encrypted_eec546499c77_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
-  gpg --fast-import --batch cd/signingkey.asc
-fi
-
-if [ -n "$TRAVIS_TAG" ]; then
+if ([ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]) || [ -n "$TRAVIS_TAG" ]; then
   openssl aes-256-cbc -K $encrypted_eec546499c77_key -iv $encrypted_eec546499c77_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
   gpg --fast-import --batch cd/signingkey.asc
 fi
--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'impl' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign --settings cd/mvnsettings.xml
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    mvn deploy --settings cd/mvnsettings.xml
 fi
 
 if [ -n "$TRAVIS_TAG" ]; then
