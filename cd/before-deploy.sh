df994670679b0bc8b322969ae3cf27dfad132590
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-22 19:57:01 +0300

    proper key filename

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'impl' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_eec546499c77_key -iv $encrypted_eec546499c77_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+  openssl aes-256-cbc -K $encrypted_eec546499c77_key -iv $encrypted_eec546499c77_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
   gpg --fast-import cd/signingkey.asc
 fi
 
 if [ -n "$TRAVIS_TAG" ]; then
-  openssl aes-256-cbc -K $encrypted_eec546499c77_key -iv $encrypted_eec546499c77_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+  openssl aes-256-cbc -K $encrypted_eec546499c77_key -iv $encrypted_eec546499c77_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
   gpg --fast-import cd/signingkey.asc
 fi
