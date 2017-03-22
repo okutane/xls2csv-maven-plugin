5f1b0596b0fc974f57d75b18ec3f04b9d238394b
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-22 22:39:56 +0300

    try batch

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'impl' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   openssl aes-256-cbc -K $encrypted_eec546499c77_key -iv $encrypted_eec546499c77_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
-  gpg --fast-import cd/signingkey.asc
+  gpg --fast-import --batch cd/signingkey.asc
 fi
 
 if [ -n "$TRAVIS_TAG" ]; then
   openssl aes-256-cbc -K $encrypted_eec546499c77_key -iv $encrypted_eec546499c77_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
-  gpg --fast-import cd/signingkey.asc
+  gpg --fast-import --batch cd/signingkey.asc
 fi
