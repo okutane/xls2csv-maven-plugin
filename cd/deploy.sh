0d621446b73748821bad8c9117d1ee127252dcd7
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-22 23:17:14 +0300

    try set version

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -4,5 +4,6 @@ if [ "$TRAVIS_BRANCH" = 'impl' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 fi
 
 if [ -n "$TRAVIS_TAG" ]; then
+    mvn org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG
     mvn deploy -P sign --settings cd/mvnsettings.xml
 fi
